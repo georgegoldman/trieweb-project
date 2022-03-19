b0af11a2ed42f37775ee4b4340890f7a76d85a4b
onyedikachi <onyedikachi@localhost.localdomain>
2022-03-19 19:01:50 +0100

    added nav bottom btn

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,124 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app style="height: 100vh" :theme="theme">
+    <v-app-bar>
+      <v-app-bar-title>
+        <v-btn>
+          <!-- <v-img :src="`https://pbs.twimg.com/profile_images/1498545478585262080/fy8Hv-Jn_400x400.jpg`"
+            :lazy-src="`https://pbs.twimg.com/profile_images/1498545478585262080/fy8Hv-Jn_400x400.jpg`"
+          >
+            <template v-slot:placeholder>
+              <v-row
+                class="fill-height ma-0"
+                align="center"
+                justify="center"
+              >
+                <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img> -->
+          <span class="material-icons">
+            account_circle
+          </span>
+        </v-btn>
+      </v-app-bar-title>
+      <v-spacer></v-spacer>
+      <v-btn >
+        <span class="material-icons-round">
+          system_update
+        </span>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col>
+            <h1>ok am in the first page</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in</h1>
+            <h1>ok am in the last text</h1>
+          </v-col>
+        </v-row>
+      </v-container>
+      <ButtomBar /> 
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import ButtomBar from '@/components/ButtomBar.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+    ButtomBar,
+  },
+
+  setup() {
+    const theme = ref('light')
+
+    return {
+      theme,
+      toggleTheme: () => theme.value = theme.value === 'light' ? 'dark' : 'light',
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ButtomBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-bottom-navigation
+    v-model="value"
+    :background-color="color"
+    dark
+    shift
+  >
+    <v-btn>
+      <v-icon>mdi-television-play</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <v-icon>mdi-music-note</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <v-icon>mdi-book</v-icon>
+    </v-btn>
+
+    <v-btn>
+      <v-icon>mdi-image</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+  export default {
+    name: "ButtomBarr",
+
+  }
+</script>
